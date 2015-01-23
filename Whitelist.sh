16d3015ccbbbb1a290f272fbcd0189289c4deb0d
Seth Rainsdon <srainsdon@nunetnetworks.net>
2015-01-23 10:08:52 -0800

    Create Whitelist.sh

---
+++ Whitelist.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -
+LIST=/root/allowed-domains.txt
+
+iptables-restore <<'END'
+*filter
+:INPUT DROP
+:FORWARD DROP
+:OUTPUT ACCEPT
+-A INPUT -i lo -j ACCEPT
+-A OUTPUT -o lo -j ACCEPT
+-A INPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+COMMIT
+END
+while read entry; do
+    case $entry in
+        *[0-9])
+        iptables -A OUTPUT -d $entry -j ACCEPT
+        ;;
+
+        *)
+        dig $entry ANY | awk '/\tA\t/ {print $5}' \
+            | xargs -iADDR iptables -A OUTPUT -d ADDR -j ACCEPT
+        ;;
+    esac
+done < $LIST
+iptables -A OUTPUT -j REJECT
